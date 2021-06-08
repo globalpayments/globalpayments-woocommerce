e9b938e2b4ee52121b3d9c0b2c95b817e94658fe
apetrovici <adina.petrovici@endava.com>
2021-06-08 10:40:05 +0300

    TR-12: [WooCommerce] add error message for missing token

--- src/Gateways/Handlers/PaymentTokenHandler.php
+++ src/Gateways/Handlers/PaymentTokenHandler.php
@@ -6,8 +6,9 @@ use GlobalPayments\WooCommercePaymentGatewayProvider\Data\PaymentTokenData;
 
 class PaymentTokenHandler extends AbstractHandler {
 	public function handle() {
-		if ( ! $this->response->token ) {
-			return;
+		if ( empty( $this->response->token ) ) {
+			wc_add_notice( __( 'Payment method token not received from gateway.', 'globalpayments-gateway-provider-for-woocommerce' ), 'error' );
+			throw new \Exception( __( 'Payment method token not received from gateway.', 'globalpayments-gateway-provider-for-woocommerce' ) );
 		}
 
 		( new PaymentTokenData( $this->request ) )->save_new_token( $this->response->token, $this->response->cardBrandTransactionId );
