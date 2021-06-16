508530dcb29ff063dfb1176d46f8140e14cce35f
apetrovici <adina.petrovici@endava.com>
2021-06-16 14:33:24 +0300

    TR-12: [WooCommerce] remove exception for empty token

--- src/Gateways/Handlers/PaymentTokenHandler.php
+++ src/Gateways/Handlers/PaymentTokenHandler.php
@@ -7,8 +7,7 @@ use GlobalPayments\WooCommercePaymentGatewayProvider\Data\PaymentTokenData;
 class PaymentTokenHandler extends AbstractHandler {
 	public function handle() {
 		if ( empty( $this->response->token ) ) {
-			wc_add_notice( __( 'Payment method token not received from gateway.', 'globalpayments-gateway-provider-for-woocommerce' ), 'error' );
-			throw new \Exception( __( 'Payment method token not received from gateway.', 'globalpayments-gateway-provider-for-woocommerce' ) );
+		    return;
 		}
 
 		( new PaymentTokenData( $this->request ) )->save_new_token( $this->response->token, $this->response->cardBrandTransactionId );
