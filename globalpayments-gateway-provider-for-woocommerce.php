17301454f251ad4b80d8ee9963585bdda9e6545d
SenthilKumar <senthilkumar.muthusamy@e-hps.com>
2022-02-22 15:36:18 +0530

    woocommerce version and php-sdk versions updated

--- globalpayments-gateway-provider-for-woocommerce.php
+++ globalpayments-gateway-provider-for-woocommerce.php
@@ -5,7 +5,7 @@
  * Description: This extension allows WooCommerce to use the available Global Payments payment gateways. All card data is tokenized using the respective gateway's tokenization service.
  * Version: 1.1.5
  * Requires PHP: 5.5.9
- * WC tested up to: 5.6.0
+ * WC tested up to: 6.0.0
  * Author: Global Payments
 */
 
