5f09228fd7b55c6524455a98a093b82c1b82db87
javiercrac <javiercrac@gmail.com>
2022-08-18 10:55:21 -0300

    Tr 13 add new hosted field for card holder name
    * TR-13 add card holder field
    
    * Use Inheritance for GPAPI
    
    * Add if for gpapi gateway
    
    * validation for card holder name only in gpapi
    
    * add require card holder name to frontend_gateway_options
    
    * remove js validation useless
    
    * aligment fix
    
    * TR-13: add card holder name to request
    
    * TR-13: add card holder name to request
    
    * TR-13: placeholder update
    
    * TR-13: changelog update
    
    * TR-13: upgrade version
    
    * TR-13: cr request
    
    * TR-13: fix for sending cardholdername to request
    
    * TR-13: adapt to other possible gateway
    
    * TR-13: remove spaces
    
    * TR-13: code refactor improvement
    
    * TR-13:  cr fix spaces
    
    * TR-13: update sdk
    
    * TR-13: update composer.lock file

--- globalpayments-gateway-provider-for-woocommerce.php
+++ globalpayments-gateway-provider-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name: GlobalPayments WooCommerce
  * Plugin URI: https://github.com/globalpayments/globalpayments-woocommerce
  * Description: This extension allows WooCommerce to use the available Global Payments payment gateways. All card data is tokenized using the respective gateway's tokenization service.
- * Version: 1.3.0
+ * Version: 1.4.0
  * Requires PHP: 7.1
  * WC tested up to: 6.6.0
  * Author: Global Payments
